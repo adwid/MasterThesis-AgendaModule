73ac7c2e6da093f874f5eb71e4634056af9c782a
adwid <adrien.widart@student.uclouvain.be>
2019-11-30 23:02:04 +0100

    Inbox/create : forward to ES

--- InboxCommand/routes/agenda.js
+++ InboxCommand/routes/agenda.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const eventStore = require("../eventStore");
 
 router.post("/", (req, res) => {
-    console.log(req.body);
-    res.end();
+    eventStore.get().communication.activity().postAgenda(req.body)
+        .failed((err, command) => {
+            console.error("Error while sending message to ES : %s", err);
+            res.status(500).end();
+        })
+        .delivered(command => {
+            res.end();
+        });
 });
 
 module.exports = router;
