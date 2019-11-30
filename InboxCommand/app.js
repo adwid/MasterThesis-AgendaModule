6c1c3c976089c2ed2b8edb8cfdf7bd347685a37c
adwid <adrien.widart@student.uclouvain.be>
2019-11-30 22:59:31 +0100

    App : connection to ES on start

--- InboxCommand/app.js
+++ InboxCommand/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const eventStore = require('./eventStore');
 
 var app = express();
+
+eventStore.connect()
+    .then(() => console.log("Connected to event store !"))
+    .catch((err) => console.error("Error with event store connection : %s", err));
 
 app.use(logger('dev'));
 app.use(express.json());
