1de3b9f400c3e26692eb6eead96f689ae00a2a49
adwid <adrien.widart@student.uclouvain.be>
2019-12-01 11:37:08 +0100

    Querier : only get 'agendaPosted' event

--- Querier/routes/agenda.js
+++ Querier/routes/agenda.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const eventStore = require('../eventStore');
 
-eventStore.get().events.observe()
+eventStore.get().events
+    .observe({
+        where: {
+            name: "agendaPosted"
+        }
+    })
     .received((e, cancel) => {
         console.log("Event received ! ");
         console.log(e);
