09b64f1284d1332d943845e36bafd8801f31cebc
adwid <adrien.widart@student.uclouvain.be>
2019-12-14 23:34:11 +0100

    QUERIER : RESET

--- Querier/app.js
+++ Querier/app.js
@@ -1,24 +1,14 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const eventStore = require('./eventStore');
 
 var app = express();
-
-eventStore.connect()
-    .catch(err => {
-        console.error("Error with event store connection : %s", err);
-        process.exit(1);
-    })
-    .then(() => {
-        console.log("Connected to event store !");
-        app.use('/agenda', require('./routes/agenda'));
-    });
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use("/agenda", require('./routes/agenda'));
 
 module.exports = app;
