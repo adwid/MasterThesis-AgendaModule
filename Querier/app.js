0b718a97617cede955862b6f0fe4b4794d623b83
adwid <adrien.widart@student.uclouvain.be>
2019-12-15 11:01:04 +0100

    Querier : connection to MongoDB

--- Querier/app.js
+++ Querier/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var database = require('./database');
 
 var app = express();
 
@@ -10,6 +11,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/agenda", require('./routes/agenda'));
+
+database.open()
+    .catch(() => {
+        console.error('Please check that the MongoDB server is running.');
+        process.exit(1);
+    });
 
 // initialize the event store subscription
 require('./handlers/eventStoreHandler');
---
+++ Querier/database.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const connectionUrl = 'mongodb://localhost/querier';
+
+mongoose.Promise = global.Promise;
+
+exports.open = function openMongoDBConnection() {
+    return mongoose.connect(connectionUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => console.log('Connected to %s', connectionUrl))
+        .catch(error => {
+            console.error('Error connecting to %s: %s', connectionUrl, error);
+            return Promise.reject(error);
+        });
+};
+
+exports.close = function closeMongoDBConnection() {
+    return mongoose.connection.close()
+        .then(() => console.log('Disconnected from %s', connectionUrl))
+        .catch(error => {
+            console.error('Failed to close connection to %s: %s', connectionUrl, error);
+            return Promise.reject(error);
+        });
+};
