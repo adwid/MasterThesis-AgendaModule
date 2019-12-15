0e8c7d2b98ad3f478bc04c167a0f65d6f5c6146b
adwid <adrien.widart@student.uclouvain.be>
2019-12-15 16:59:07 +0100

    Inbox : POST vote and to ES

--- Inbox/handlers/eventStoreHandler.js
+++ Inbox/handlers/eventStoreHandler.js
@@ -5,20 +5,21 @@ const eventStoreClient = require('event-store-client');
 
 const streamID = "agenda";
 
-function postNewAgenda(agenda) {
+function postEvent(content, type) {
     const event = {
         eventId: eventStoreManager.getNewID(),
-        eventType: 'newAgenda',
-        data: agenda
+        eventType: type,
+        data: content
     };
     write("agenda", [event]);
 }
 
 function write(streamID, eventsArray) {
+    // todo promise, catch etc for the http request response
     eventStore.writeEvents(streamID, eventStoreManager.getExpectedVersion(), false, eventsArray, esCredentials, completed => {
         console.log("Event written result: " + eventStoreClient.OperationResult.getName(completed.result));
     });
 }
 
-module.exports = {postNewAgenda};
+module.exports = {postEvent};
 
--- Inbox/routes/agenda.js
+++ Inbox/routes/agenda.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const esHandler = require('../handlers/eventStoreHandler');
 
-router.post("/post", (req, res) => {
-    esHandler.postNewAgenda(req.body);
+router.post("/", (req, res) => {
+    esHandler.postEvent(req.body, "newAgenda");
+    res.end();
+});
+
+router.post("/vote", (req, res) => {
+    esHandler.postEvent(req.body, "vote");
     res.end();
 });
 
